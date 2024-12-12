af2f96ac2db2f26dbc7ed1768ba50d37b5520a17
JShistory <behappy0916@naver.com>
2024-12-12 17:44:39 +0900

    [feat] kakaoMapService 추가

---
+++ src/main/java/com/october/back/photobooth/service/KakaoMapService.java
@@ -0,0 +1,27 @@
+package com.october.back.photobooth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Service
+public class KakaoMapService {
+    private final WebClient kakaoClient;
+
+    public Mono<String> searchByKeyword(String keyword, double x, double y, int radius) {
+        return kakaoClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v2/local/search/keyword.json")
+                        .queryParam("query", keyword)
+                        .queryParam("x", x)
+                        .queryParam("y", y)
+                        .queryParam("radius", radius)
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class); // 필요시 DTO로 매핑 가능
+    }
+}
