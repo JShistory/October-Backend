3d40d2de125c9163f94bc211ac4373ce38ba64eb
JShistory <behappy0916@naver.com>
2024-12-12 16:50:14 +0900

    [feat] photobooth 리포지토리 추가

---
+++ src/main/java/com/october/back/photobooth/repository/PhotoBoothRepository.java
@@ -0,0 +1,7 @@
+package com.october.back.photobooth.repository;
+
+import com.october.back.photobooth.entity.PhotoBooth;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PhotoBoothRepository extends JpaRepository<PhotoBooth, Long> {
+}
