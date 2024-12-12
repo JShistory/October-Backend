d80a14b80420f233492857750f0018fdb4901651
JShistory <behappy0916@naver.com>
2024-12-12 16:44:29 +0900

    [feat] photoBooth 엔티티 추가

---
+++ src/main/java/com/october/back/photoBooth/entity/PhotoBooth.java
@@ -0,0 +1,28 @@
+package com.october.back.photoBooth.entity;
+
+import com.october.back.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class PhotoBooth extends BaseEntity {
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+    @Column(name = "brandName", nullable = false)
+    private String brandName;
+
+    @Builder
+    private PhotoBooth(double latitude, double longitude, String brandName) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.brandName = brandName;
+    }
+}
