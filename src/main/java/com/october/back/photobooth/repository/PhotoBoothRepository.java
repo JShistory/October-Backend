1e63909918572bf40b019b5f054351abdbc6c624
JShistory <behappy0916@naver.com>
2025-01-07 19:26:13 +0900

    [feat] findByBrandName 기능 추가

--- src/main/java/com/october/back/photobooth/repository/PhotoBoothRepository.java
+++ src/main/java/com/october/back/photobooth/repository/PhotoBoothRepository.java
@@ -1,9 +1,12 @@
 package com.october.back.photobooth.repository;
 
+import com.october.back.photobooth.entity.BrandType;
 import com.october.back.photobooth.entity.PhotoBooth;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoBoothRepository extends JpaRepository<PhotoBooth, Long> {
     PhotoBooth findByPlaceNameAndAddress(String placeName, String address);
     boolean existsByPlaceName(String placeName);
+    List<PhotoBooth> findByBrandName(BrandType brandName);
 }
