3be1fbd84ecab6d4baf87a56af8af966fb75e647
JShistory <behappy0916@naver.com>
2025-01-08 17:43:43 +0900

    [feat] photoBooth 컨트롤러 추가

--- src/main/java/com/october/back/photobooth/controller/PhotoBoothController.java
+++ src/main/java/com/october/back/photobooth/controller/PhotoBoothController.java
@@ -1,22 +1,10 @@
 package com.october.back.photobooth.controller;
 
-import com.october.back.photobooth.service.KakaoMapService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/photobooth")
 @RestController
 public class PhotoBoothController {
-    private final KakaoMapService kakaoMapService;
-
-    @GetMapping("/kakao")
-    public String getPhotoBoothKeyword(@RequestParam String keyword) {
-
-        kakaoMapService.searchAndSaveAll(keyword);
-        return "kakao Map 호출 서비스입니다.";
-    }
 }
